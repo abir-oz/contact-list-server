c8b43c208f7f96b9a3058940cbf20eb858c2277f
Abir s <abir-oz>
2021-09-27 03:18:21 +0600

    done

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Hello World' });
@@ -9,7 +16,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contact', require('./routes/contact'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
